bff46b1eeaf98d39f428cd59ab07f10d9679abae
Manish Mishra <manishmishra786876@gmail.com>
2022-05-16 11:24:42 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('This is client side javascript')
+
+const weatherForm=document.querySelector('form')
+const search=document.querySelector('input')
+let messageOne=document.querySelector('#message-1')
+let messageTwo=document.querySelector('#message-2')
+
+// messageOne.textContent='From Javascript'
+
+weatherForm.addEventListener('submit',(event)=>{
+    event.preventDefault()
+
+    const location=search.value
+    
+    messageOne.textContent='loading...'
+    messageTwo.textContent=''
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageOne.textContent=''
+            return messageTwo.textContent=data.error
+        }
+        messageOne.textContent=data.location
+        messageTwo.textContent=data.forecast
+    })
+})
+})
